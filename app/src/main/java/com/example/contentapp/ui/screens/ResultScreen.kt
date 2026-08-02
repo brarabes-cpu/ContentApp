22ac83f69fd97e53f27cb8818371d9af1c33ad7a
rabes <bra.rabes@gmail.com>
2026-08-02 18:30:56 +0300

    fix: ContentScale import package

--- app/src/main/java/com/example/contentapp/ui/screens/ResultScreen.kt
+++ app/src/main/java/com/example/contentapp/ui/screens/ResultScreen.kt
@@ -10,7 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ContentScale
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
